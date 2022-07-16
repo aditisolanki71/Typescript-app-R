96b8fa614b935894db672b79eef4b2dd00932732
Unknown <adtsolanki22@gmail.com>
2022-07-16 16:46:13 +0530

    Add and Delete Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './Model/todo.model'
 //type provide by react
 //FunctionComponent = FC
 const App: React.FC = () => {
-  const todos = [
+const [todos,setTodos] = useState<Todo[]>(
+  [ 
     {id:'1', text: 'Typescript'},
     {id:'2', text: 'GraphQL'},
-    {id:'3', text: 'React'},
- ];
+    {id:'3', text: 'React'}
+  ]);
+ const submitTodo = (newText:string) => {
+   const newTodo = {id: Math.random().toString(),text:newText};
+  //  setTodos([...todos,newTodo]);
+  setTodos(prevTodos => [...prevTodos,newTodo]);
+ }
+ const deleteTodo = (id:string) => {
+ //  setTodos([...todos,newTodo]);
+ setTodos(prevTodos => {
+   return prevTodos.filter(todo => todo.id !== id);
+ });
+}
   return (
     <div className="App">
       Typescript React
-      <TodoList items={todos} />
+      <NewTodo onAddTodo={submitTodo} />
+      <TodoList items={todos} onDeleteTodo={deleteTodo}/>
     </div>
   );
 };
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import React, {useRef} from "react";
+interface NewTodoProps {
+   onAddTodo: (todoText:string)  => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+   const textInputRef = useRef<HTMLInputElement>(null);
+   const handleSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      const enteredText = textInputRef.current!.value;
+      console.log("entered",enteredText);
+      props.onAddTodo(enteredText);
+   }
+   return (
+      <div>
+         <form onSubmit={handleSubmit}>
+            <div>
+            <label htmlFor="todo-text">Todo Text</label>
+            <input type="text" id="todo-text" ref={textInputRef}></input>
+            </div>
+            <button type="submit">Add Todo</button>
+         </form>
+      </div>
+   )
+}
+export default NewTodo;
