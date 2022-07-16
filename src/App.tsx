75689886e93a60f95a40bc93494d0c3dae930d48
Unknown <adtsolanki22@gmail.com>
2022-07-16 16:07:34 +0530

    Display Todo List

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+//type provide by react
+//FunctionComponent = FC
 const App: React.FC = () => {
+  const todos = [
+    {id:'1', text: 'Typescript'},
+    {id:'2', text: 'GraphQL'},
+    {id:'3', text: 'React'},
+ ];
   return (
     <div className="App">
       Typescript React
+      <TodoList items={todos} />
     </div>
   );
 };
